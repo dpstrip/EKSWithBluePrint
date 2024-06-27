097296300b9deb6a4e579273565a828d91e2a2d1
David Stripeik <dpstrip@gmail.com>
2024-06-27 13:12:29 -0500

    Have it working with no nodes

--- bin/my-blueprint.ts
+++ bin/my-blueprint.ts
@@ -1,21 +1,54 @@
-#!/usr/bin/env node
+// #!/usr/bin/env node
+// import 'source-map-support/register';
+// import * as cdk from 'aws-cdk-lib';
+// import { MyBlueprintStack } from '../lib/my-blueprint-stack';
+
+// const app = new cdk.App();
+// new MyBlueprintStack(app, 'MyBlueprintStack', {
+//   /* If you don't specify 'env', this stack will be environment-agnostic.
+//    * Account/Region-dependent features and context lookups will not work,
+//    * but a single synthesized template can be deployed anywhere. */
+
+//   /* Uncomment the next line to specialize this stack for the AWS Account
+//    * and Region that are implied by the current CLI configuration. */
+//    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+
+//   /* Uncomment the next line if you know exactly what Account and Region you
+//    * want to deploy the stack to. */
+//   // env: { account: '123456789012', region: 'us-east-1' },
+
+//   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+// });
+
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { MyBlueprintStack } from '../lib/my-blueprint-stack';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
 
 const app = new cdk.App();
-new MyBlueprintStack(app, 'MyBlueprintStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// AddOns for the cluster.
+const addOns: Array<blueprints.ClusterAddOn> = [
+    new blueprints.addons.ArgoCDAddOn,
+    new blueprints.addons.CalicoOperatorAddOn,
+    new blueprints.addons.MetricsServerAddOn,
+    new blueprints.addons.ClusterAutoScalerAddOn,
+    //new blueprints.addons.ContainerInsightsAddOn,
+    new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+    new blueprints.addons.VpcCniAddOn(),
+    new blueprints.addons.CoreDnsAddOn(),
+    new blueprints.addons.KubeProxyAddOn(),
+    new blueprints.addons.EfsCsiDriverAddOn(),
+   // new blueprints.addons.XrayAddOn()
+];
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const account = '929556976395';
+const region = 'us-east-1';
+const version = 'auto';
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const stack = blueprints.EksBlueprint.builder()
+    .account(account)
+    .region(region)
+    .version(version)
+    .addOns(...addOns)
+    .build(app, 'eks-blueprint');
+// do something with stack or drop this variable
--- lib/my-blueprint-stack.ts
+++ lib/my-blueprint-stack.ts
@@ -1,16 +1,37 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+// import * as cdk from 'aws-cdk-lib';
+// import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class MyBlueprintStack extends cdk.Stack {
+// export class MyBlueprintStack extends cdk.Stack {
+//   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//     super(scope, id, props);
+
+//     // The code that defines your stack goes here
+
+//     // example resource
+//     // const queue = new sqs.Queue(this, 'MyBlueprintQueue', {
+//     //   visibilityTimeout: cdk.Duration.seconds(300)
+//     // });
+//   }
+// }
+// lib/my-eks-blueprints-stack.ts
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+
+export default class MyBlueprintStack extends Construct {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id);
 
-    // The code that defines your stack goes here
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'MyBlueprintQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const blueprint = blueprints.EksBlueprint.builder()
+    .version('auto')
+    .account(account)
+    .region(region)
+    .addOns()
+    .teams()
+    .build(scope, id+'-stack');
   }
 }
